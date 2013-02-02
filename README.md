f0891e67b4e206ee6861a218be95d6edfddd76f7
Alexandre Kaspar <xion.luhnis@gmail.com>
2013-02-03 06:51:29 +0900

    Base version of zigfy

--- README.md
+++ README.md
@@ -2,3 +2,79 @@ zigfy
 =====
 
 Z Image Gallery JQuery extension
+
+What you need
+=============
+`zigfy` is a JQuery plugin. Thus you obviously want to have JQuery (and you should include it before including "zigfy").
+You need to include the CSS and Javascript files (as well as image resources if you want to use the same):
+  * `jquery.zigfy.js` - the actual Javascript extension
+  * `zigfy.css` - the basic style for "zigfy"
+
+And that's it!
+
+How to use it
+=============
+There are two different ways to use `zigfy`:
+
+1. The Javascript Way
+=====================
+By calling it in your Javascript code (when the DOM is ready of course):
+
+```Javascript
+$(function(){
+  $('.mycontainer').zigfy({ /* my options */ });
+});
+```
+
+Thus you can set parameters and configure each instance you want to create separately.
+
+2. The HTML Way
+===============
+By adding the class "zigfy" to the gallery containers (probably divs), and having them contain all the images you want (as `<img src="path" />` HTML nodes of course).
+
+Simply, "zigfy" runs by default the Javascript code above for all elements selected by `$('.zigfy')`. That's it, nothing miracular here.
+
+Options
+=======
+Here are the options you can pass to "zigfy" (with their default value):
+  * ""resize"": "true" - whether to respond to resize events automatically
+  * ""layout"": "'maximize'" - the layout to use
+    * "maximize" stretches the image so that it cover the whole container
+    * "full" puts the whole image in the container, whatever its size
+  * ""align": "'center'" - the alignment, one from:
+    * "topleft", 
+    * "top",
+    * "topright",
+    * "left",
+    * "center",
+    * "right",
+    * "bottomleft",
+    * "bottom",
+    * "bottomright"
+  * ""transition"": "'fade'" - the transition to use when navigating between images, one from:
+    * "fade",
+    * "flash",
+    * or an object with the three following function: `{init, before, after}`, see jquery.zigfy.js implementation of "fade" and "flash" to make your own
+  * ""showNav"": "true" - whether to show the left / right navigation buttons
+  * ""padding"": "10" - the overall padding for images in layout "full", it makes sure that there is this amount of available, taking CSS borders into account
+  * ""mapMode"": "true" -  whether to enable the "map" mode, i.e. clicking to grab and see more, only for the "maximize" layout
+  * ""imgSelector"": "null" - the image selector to use instead of the element's images
+
+Examples
+========
+That's ... soon coming!
+
+Future things
+=============
+I am thinking of adding an auto-navigation (like gallery auto-play) mode, with possibly a bar showing the timing.
+This would be the next feature.
+
+But of course, bugs are important.
+
+License
+=======
+The code is released under the MIT license, as is.
+
+Others
+======
+If you have questions, request or saw issues, don't hesitate.
---
+++ js/jquery.zigfy.js
@@ -0,0 +1,528 @@
+// zigfy, Z Image Gallery plugin for JQuery
+// version 0.1
+// (c) 2013 Alexandre Kaspar [xion.luhnis@gmail.com]
+// released under the MIT license
+
+(function ($) {
+
+  // the available image transitions
+  var transitions = {
+    /**
+     * Alpha transition
+     */
+    fade: {
+      init: function ( /* length */ ) {},
+      before: function () {},
+      after: function () {
+        var self = this;
+        self.$el.scrollTop(0);
+        for (var i = 0; i < self.images.length; ++i) {
+          var $img = self.images[i];
+          // fade animation
+          if (i === self.index) $img.stop().fadeIn(500);
+          else $img.stop().fadeOut(500);
+        }
+      }
+    },
+    /**
+     * Exposure from white
+     */
+    flash: {
+      init: function ( /* length */ ) {},
+      before: function () {
+        var self = this;
+        // we add the white cover
+        if (self.lastIndex !== -1 && self.isLoaded(self.lastIndex)) {
+          self.$cover.addClass('zigfy-flash').show();
+        }
+        // hide all images first
+        for (var i = 0; i < self.images.length; ++i) self.images[i].hide();
+      },
+      after: function () {
+        var self = this;
+        // show current image (behind white cover)
+        self.images[self.index].show();
+        // we fade out the white cover
+        self.$cover.fadeOut(500, function () {
+          self.$cover.removeClass('zigfy-flash');
+        });
+      }
+    }
+    // end of transitions
+  };
+
+  function Zigfy(element, options) {
+    var self = this;
+    var $el = this.$el = $(element);
+    this.options = options;
+
+    // finding the images
+    var $imgs;
+    if (options.imgSelector) {
+      $imgs = $(options.imgSelector);
+    } else {
+      $imgs = $el.find('img');
+    }
+    // current index
+    self.index = 0;
+    self.lastIndex = -1;
+
+    // layout
+    var layout;
+    if (typeof options.layout == 'function') layout = options.layout;
+    else layout = self[options.layout + 'Layout'] || self.maximize;
+    self.layout = layout.bind(self);
+
+    // alignment
+    if (['topleft', 'top', 'topright', 'left', 'center', 'right', 'bottomleft', 'bottom', 'bottomright'].indexOf(options.align) === -1) {
+      options.align = 'topleft';
+    }
+    var align = options.align;
+    self.alignX = align.indexOf('left') !== -1 ? -1 : (align.indexOf('right') !== -1 ? 1 : 0);
+    self.alignY = align.indexOf('top') !== -1 ? -1 : (align.indexOf('bottom') !== -1 ? 1 : 0);
+
+    // general cover layer
+    self.$cover = $('<div />').css({
+      'display': 'none',
+      'z-index': $imgs.length + 3
+    }).addClass('zigfy-cover');
+
+    // init transition
+    var trans;
+    if (typeof options.transition == 'object') {
+      trans = options.transition;
+      if (!trans.init) trans.init = function () {};
+      if (!trans.before) trans.before = function () {};
+      if (!trans.after) trans.after = function () {};
+    } else {
+      trans = transitions[options.transition] || transitions.fade;
+    }
+    self.transition = trans;
+    trans.init.call(self, $imgs.length);
+
+    // populating the gallery
+    var images = self.images = [];
+    var loaded = self.loaded = {};
+    var dims = self.dims = {};
+    $el.children().remove();
+    $imgs.each(function () {
+      var $origImg = $(this);
+      var index = images.length;
+      // pre-init stuff
+      if (index === self.index) self.preInit();
+
+      // image creation
+      var $img = $('<img />').data('index', index).css({
+        'display': index === self.index ? 'block' : 'none', // we only show the first one by default
+        'height': 'auto',
+        'width': 'auto',
+        'z-index': index + 1
+      }).attr('src', $origImg.attr('src'));
+      images.push($img);
+
+      // image loading
+      var onLoad = function () {
+        loaded[index] = true;
+        dims[index] = {
+          height: $img.height(),
+          width: $img.width()
+        };
+        // mapMode = map navigation on click
+        if (options.mapMode) {
+          $img.click(function (e) {
+            e.preventDefault();
+            self.toggleNavigation();
+          });
+        }
+
+        // are we the target image?
+        if (index === self.index) {
+          // we can update the layout
+          self.layout();
+          // and do the post-init stuff
+          self.postInit();
+        }
+
+        // done loading
+      };
+      // onLoad hook
+      if (self.isLoaded(index)) onLoad();
+      else $img.load(onLoad);
+
+      // image in the DOM
+      $el.append($img);
+    });
+
+    // navigation
+    if (options.showNav && images.length > 1) {
+      // images are from z-index = 1 to images.length
+      // => prev: images.length + 1
+      this.navPrev = $('<div class="zigfy-prev" />').css('z-index', images.length + 1).click(function () {
+        self.prev();
+      }).appendTo($el);
+      // => next: images.length + 2
+      this.navNext = $('<div class="zigfy-next" />').css('z-index', images.length + 2).click(function () {
+        self.next();
+      }).appendTo($el);
+    }
+    // mapMode
+    if (options.mapMode) $el.append(self.$cover);
+
+    // events
+    if (options.resize) {
+      $(window).resize(function () {
+        self.layout();
+      });
+    }
+  }
+
+  Zigfy.prototype = {
+
+    /**
+     * Loaded check
+     */
+    isLoaded: function (index) {
+      var self = this;
+      if (index === undefined) index = self.index;
+
+      // the normal way
+      if ( !! self.loaded[index]) return true;
+
+      // the hack way for other cases (cache, error)
+      var $img = self.images[index];
+      return $img.readyState || $img.complete;
+    },
+
+    /**
+     * Maximize layout which stretches
+     * the smallest ratio to fit 100%
+     * @param $cnt the container
+     * @param $img the image to lay out
+     */
+    maximizeLayout: function () {
+      var self = this;
+      // the container
+      var $cnt = self.$el;
+      // the image
+      var $img = self.images[self.index];
+
+      // if there is no image, it's done
+      if (!$img || !self.isLoaded()) return;
+
+      // we check whether it's not big enough
+      var W = $cnt.width();
+      var H = $cnt.height();
+
+      // default css
+      // Note: we only use top/left, not right/bottom!
+      $img.css({
+        'border': 0,
+        'margin': 0,
+        'position': 'absolute',
+        'height': 'auto',
+        'width': 'auto'
+      });
+
+      // the ratios
+      var d = self.dims[self.index];
+      var fx = d.width / W;
+      var fy = d.height / H;
+      if (fx < 1) {
+        if (fy < 1) {
+          // both w/h too small
+          // => we stretch the smallest ratio to 100%
+          // => full area will be covered
+          if (fx > fy) {
+            $img.css('height', H); // 100%
+          } else {
+            $img.css('width', W);
+          }
+        } else {
+          // only w too small
+          $img.css('width', W);
+        }
+      } else {
+        // width is ok
+        if (fy < 1) {
+          // only h too small
+          $img.css('height', H);
+        } else {
+          // both w/h ok
+          // => we stretch the smallest ratio to 100%
+          if (fx > fy) {
+            $img.css('height', H);
+          } else {
+            $img.css('width', W);
+          }
+        }
+      }
+
+      // the new image dimensions
+      var w = $img.width();
+      var h = $img.height();
+
+      // alignment X
+      var left;
+      switch (self.alignX) {
+        case -1:
+          left = 0;
+          break;
+        case 0:
+          left = Math.floor((W - w) / 2);
+          break;
+        case 1:
+          left = W - w;
+          break;
+      }
+      $img.css('left', left);
+      // alignment Y
+      var top;
+      switch (self.alignY) {
+        case -1:
+          top = 0;
+          break;
+        case 0:
+          top = Math.floor((H - h) / 2);
+          break;
+        case 1:
+          top = H - h;
+          break;
+      }
+      $img.css('top', top);
+    },
+
+    /**
+     * Special layout for the panel
+     * which fits the image in the remaining
+     * available space using the same
+     * rules as the custom layout
+     */
+    fullLayout: function () {
+      var self = this;
+      var $cnt = self.$el;
+      var $img = self.images[self.index];
+
+      // we need an image to lay out
+      if (!$img || !self.isLoaded()) return;
+
+      // dimensions
+      var d = self.dims[self.index];
+      var w = d.width;
+      var h = d.height;
+      var W = $cnt.width();
+      var H = $cnt.height();
+      var padding = self.options.padding || 0;
+
+      // we use the ratios
+      var b = function (what) {
+        return parseInt($img.css('border-' + what + '-width').replace('px', ''), 10);
+      };
+      var horizPad = 2 * padding + b('left') + b('right');
+      var vertiPad = 2 * padding + b('top') + b('bottom');
+      var fx = w / (W - horizPad);
+      var fy = h / (H - vertiPad);
+
+      // defaults
+      $img.css({
+        'position': 'absolute',
+        'height': 'auto',
+        'width': 'auto',
+        'top': 0,
+        'left': 0,
+        'margin': 0
+      });
+
+      /**
+       * Invariant :
+       *  the aspect ratio w/h = ( W - horizPad )/( H - vertiPad )
+       */
+
+      // XXX take alignX and alignY into consideration...
+      // checking ratios
+      if (fx < 1) {
+        // w too small
+        // we need to center the whole image
+        if (fy < 1) { // h too small too
+          // centering vertically
+          $img.css('height', H - vertiPad);
+          $img.css('top', padding + (H - h) / 2);
+          $img.css('width', W - horizPad);
+          $img.css('left', padding + (W - w / fy) / 2 - b('left'));
+        } else { // h ok
+          $img.css('height', H - vertiPad);
+          $img.css('top', padding);
+          $img.css('width', w / fy);
+          $img.css('left', padding + (W - w / fy) / 2 - b('left'));
+        }
+      } else {
+        // w ok, we shrink ratio
+        var FY = fy / fx;
+        if (FY < 1) { // h too small now
+          $img.css('width', w / fx);
+          $img.css('left', padding);
+          $img.css('height', h / fx);
+          $img.css('top', (H - h / fx) / 2 - b('top'));
+        } else {
+          // we shrink w since h / fx is fine
+          $img.css('height', H - vertiPad);
+          $img.css('top', padding);
+          $img.css('width', w / fy);
+          $img.css('left', (W - w / fy) / 2 - b('left'));
+        }
+      }
+    },
+
+    preInit: function () {
+      this.transition.before.call(this, this.index, this.lastIndex);
+    },
+
+    postInit: function () {
+      this.transition.after.call(this, this.index, this.lastIndex);
+    },
+
+    prev: function () {
+      var self = this;
+      var i = self.lastIndex = self.index;
+      self.index = i <= 0 ? self.images.length - 1 : i - 1;
+      self.preInit();
+      self.layout();
+      if (self.isLoaded(self.index)) self.postInit();
+    },
+
+    next: function () {
+      var self = this;
+      var i = self.lastIndex = self.index;
+      self.index = i >= self.images.length - 1 ? 0 : i + 1;
+      self.preInit();
+      self.layout();
+      if (self.isLoaded(self.index)) self.postInit();
+    },
+
+    /**
+     * Toggle the maps navigation
+     */
+    toggleNavigation: function () {
+      var self = this;
+      var $cover = self.$cover;
+      // current mode?
+      if (self.mapMode) {
+        // we hide the cover
+        $cover.stop().fadeOut(500, function () {
+          $cover.removeClass('zigfy-maps');
+        });
+
+        // and stop the navigation
+        $cover.unbind('mousemove');
+        $cover.unbind('click');
+      } else {
+        // we set the click back
+        $cover.bind('click', self.toggleNavigation.bind(self));
+        // we show the cover
+        $cover.addClass('zigfy-maps').stop().fadeIn(500);
+        $cover.css('cursor', 'move');
+        // and bind the navigation event
+        $cover.bind('mousemove', self.navigate.bind(self));
+      }
+
+      // we switch the mode
+      self.mapMode = !self.mapMode;
+      console.log('Maps: ' + self.mapMode);
+    },
+
+    distF: function (t) { // easeInOutCubic
+      // t in [0;1];
+      if ((t *= 2) < 1) { // t in [0;0.5]*2
+        return t * t * t / 2;
+      }
+      // t in [1;2]
+      t -= 2; // t in [-1;0]
+      return (t * t * t + 2) / 2;
+    },
+
+    navigate: function (e) {
+      var self = this;
+      var $cnt = self.$el;
+      var W = $cnt.width();
+      var H = $cnt.height();
+      var $img = self.images[self.index];
+      var w = $img.width();
+      var h = $img.height();
+      var range, scroll;
+
+      // the gap in y
+      if (h > H) {
+        var fy = e.pageY / H;
+        range = h - H;
+        scroll = range * self.distF(fy);
+        $img.css('top', -scroll);
+      }
+      // the gap in x
+      if (w > W) {
+        var fx = e.pageX / W;
+        range = w - W;
+        scroll = range * self.distF(fx);
+        $img.css('left', -scroll);
+      }
+    }
+
+  };
+
+  // the zigfy JQuery function
+  $.fn.zigfy = function (options) {
+
+    if (options === true) {
+      return this.data('zigfy');
+    } else if (typeof options == 'string') {
+      var zigfy = this.data('zigfy');
+      if (zigfy) zigfy[options]();
+      return this;
+    }
+
+    options = $.extend({}, $.fn.zigfy.defaults, options);
+
+    function get(ele) {
+      var zigfy = $.data(ele, 'zigfy');
+      if (!zigfy) {
+        zigfy = new Zigfy(ele, $.fn.zigfy.elementOptions(ele, options));
+        $.data(ele, 'zigfy', zigfy);
+      }
+      return zigfy;
+    }
+
+    this.each(function () {
+      get(this);
+    });
+
+    return this;
+  };
+
+  $.fn.zigfy.defaults = {
+    resize: true, // whether to respond to resize events automatically
+    layout: 'maximize', // or 'full' to show the full picture, in reduced size
+    align: 'center', // or 'topleft', 'top', 'topright', 'left', 'right', 'bottomleft', 'bottom', 'bottomright'
+    transition: 'fade', // or 'flash', or {init, before, after}
+    showNav: true, // for the left / right navigation buttons
+    autoNav: false, // whether to have automatic navigation
+    autoNavBar: false, // whether to show a countdown navigation bar (only when autoNav == true
+    autoNavDuration: 5000, // timing for autoNav == true
+    padding: 10, // to override CSS padding in 'full' mode, not used in 'zoom' mode
+    mapMode: true, // whether to enable clicking to grab and see more, only for the 'zoom' mode
+    imgSelector: null // the image selector (null => img in HTML target)
+  };
+
+  // Overwrite this method to provide options on a per-element basis.
+  // For example, you could store the gravity in a 'zigfy-gravity' attribute:
+  // return $.extend({}, options, {gravity: $(ele).attr('zigfy-gravity') || 'n' });
+  // (remember - do not modify 'options' in place!)
+  $.fn.zigfy.elementOptions = function (ele, options) {
+    return $.metadata ? $.extend({}, options, $(ele).metadata()) : options;
+  };
+
+  //
+  // Apply on elements with class .zigfy
+  //
+  $(function () {
+    $('.zigfy').zigfy();
+  });
+
+})(jQuery);
